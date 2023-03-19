1d7aaa31d8b4b350e99bd088cbc08b8c6f0a9fda
rayniel95 <rainyel.ramos@gmail.com>
2023-03-19 18:31:15 -0400

    add add todo method

---
+++ contracts/Todo.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.9;
+
+contract Todo {
+    uint internal id = 0;
+    mapping (address => mapping (uint => string)) internal todos;
+    constructor() {}
+
+    function addTodo(string memory text) public {
+        todos[msg.sender][id] = text;
+        id++;
+    }
+    function editTodo(uint id, string memory newText) public {
+        require(todos[msg.sender][id])
+    }
+    function getTodo() public view returns (TodoTask memory) {
+        
+    }
+    function getTodos() public view returns (TodoTask[] memory) {
+        
+    }
+    // NOTE - if delete array does not delete the element,
+    // how is possible to save storage using the not deleted
+    // elements?
+    function deleteTodo() public {
+        
+    }
+}
---
+++ test/Todo.ts
@@ -0,0 +1,1 @@
+//TODO - search for a good tester
