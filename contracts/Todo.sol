4aee8999d12311d1fed047da9185fd4cafe89605
rayniel95 <rainyel.ramos@gmail.com>
2023-03-20 23:10:00 -0400

    add get todos method

--- contracts/Todo.sol
+++ contracts/Todo.sol
@@ -4,6 +4,10 @@ pragma solidity ^0.8.9;
 contract Todo {
     uint internal id = 0;
     mapping (address => mapping (uint => string)) internal todos;
+    struct Tuple {
+        uint id;
+        string text;
+    }
     event TodoAdded(address _address, uint _id);
     event TodoEdited(address _address, uint _id);
     event TodoDeleted(address _address, uint _id);
@@ -30,8 +34,16 @@ contract Todo {
         );
         return todos[msg.sender][_id];
     }
-    function getTodos() public view returns (mapping (uint => string) memory) {
-        return todos[msg.sender];
+    function getTodos() public view returns (Tuple[] memory) {
+        Tuple[] memory result = new Tuple[](id+1);
+        uint index;
+        for (uint _id = 0; _id <= id; _id++) {
+            if (bytes(todos[msg.sender][_id]).length != 0) {
+                result[index] = Tuple(_id, todos[msg.sender][_id]);
+                index++;
+            }
+        }
+        return result;
     }
     // NOTE - if delete array does not delete the element,
     // how is possible to save storage using the not deleted
