9d4f413414bdc69873acba2e3221f5dfa83c7443
rayniel95 <rainyel.ramos@gmail.com>
2023-03-29 22:17:31 -0400

    add test to event

--- test/Todo_unit_testing.ts
+++ test/Todo_unit_testing.ts
@@ -2,89 +2,104 @@ import { time, loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import { expect } from "chai";
 import { ethers } from "hardhat";
-//TODO - add unit testing, integration testing and automatic
-// fuzzy testing. maybe do a bit research about automatic 
-// testing
+
 describe("Todo", function () {
+  async function deployTodoFixture() {
+    // Get the ContractFactory and Signers here.
+    const TodoFactory = await ethers.getContractFactory("Todo");
+    const [owner, addr1, addr2] = await ethers.getSigners();
 
+    // To deploy our contract, we just have to call Token.deploy() and await
+    // its deployed() method, which happens once its transaction has been
+    // mined.
+    const Todo = await TodoFactory.deploy();
+
+    await Todo.deployed();
+
+    // Fixtures can return anything you consider useful for your tests
+    return { TodoFactory, Todo, owner, addr1, addr2 };
+  }
   describe("addTodo", function () {
-    it("Should set the right unlockTime", async function () {
-      const { lock, unlockTime } = await loadFixture(deployOneYearLockFixture);
-
-      expect(await lock.unlockTime()).to.equal(unlockTime);
+    it("Should emit added todo event", async function () {
+      const { Todo, owner } = await loadFixture(deployTodoFixture);
+      
+      await expect(Todo.addTodo("some todo")).to.emit(
+        Todo, "TodoAdded"
+      ).withArgs(owner.address, 0)
+      
     });
 
-    it("Should set the right owner", async function () {
-      const { lock, owner } = await loadFixture(deployOneYearLockFixture);
+  //   it("Should set the right owner", async function () {
+  //     const { lock, owner } = await loadFixture(deployOneYearLockFixture);
 
-      expect(await lock.owner()).to.equal(owner.address);
-    });
+  //     expect(await lock.owner()).to.equal(owner.address);
+  //   });
 
-    it("Should receive and store the funds to lock", async function () {
-      const { lock, lockedAmount } = await loadFixture(
-        deployOneYearLockFixture
-      );
+  //   it("Should receive and store the funds to lock", async function () {
+  //     const { lock, lockedAmount } = await loadFixture(
+  //       deployOneYearLockFixture
+  //     );
 
-      expect(await ethers.provider.getBalance(lock.address)).to.equal(
-        lockedAmount
-      );
-    });
+  //     expect(await ethers.provider.getBalance(lock.address)).to.equal(
+  //       lockedAmount
+  //     );
+  //   });
 
-    it("Should fail if the unlockTime is not in the future", async function () {
-      // We don't use the fixture here because we want a different deployment
-      const latestTime = await time.latest();
-      const Lock = await ethers.getContractFactory("Lock");
-      await expect(Lock.deploy(latestTime, { value: 1 })).to.be.revertedWith(
-        "Unlock time should be in the future"
-      );
-    });
+  //   it("Should fail if the unlockTime is not in the future", async function () {
+  //     // We don't use the fixture here because we want a different deployment
+  //     const latestTime = await time.latest();
+  //     const Lock = await ethers.getContractFactory("Lock");
+  //     await expect(Lock.deploy(latestTime, { value: 1 })).to.be.revertedWith(
+  //       "Unlock time should be in the future"
+  //     );
+  //   });
   });
 
-  describe("editTodo", function () {
-    it("Should receive and store the funds to lock", async function () {
-      const { lock, lockedAmount } = await loadFixture(
-        deployOneYearLockFixture
-      );
+  // describe("editTodo", function () {
+  //   it("Should receive and store the funds to lock", async function () {
+  //     const { lock, lockedAmount } = await loadFixture(
+  //       deployOneYearLockFixture
+  //     );
 
-      expect(await ethers.provider.getBalance(lock.address)).to.equal(
-        lockedAmount
-      );
-    });
-  });
+  //     expect(await ethers.provider.getBalance(lock.address)).to.equal(
+  //       lockedAmount
+  //     );
+  //   });
+  // });
 
-  describe("getTodo", function () {
-    it("Should receive and store the funds to lock", async function () {
-      const { lock, lockedAmount } = await loadFixture(
-        deployOneYearLockFixture
-      );
+  // describe("getTodo", function () {
+  //   it("Should receive and store the funds to lock", async function () {
+  //     const { lock, lockedAmount } = await loadFixture(
+  //       deployOneYearLockFixture
+  //     );
 
-      expect(await ethers.provider.getBalance(lock.address)).to.equal(
-        lockedAmount
-      );
-    });
-  });
+  //     expect(await ethers.provider.getBalance(lock.address)).to.equal(
+  //       lockedAmount
+  //     );
+  //   });
+  // });
 
-  describe("getTodos", function () {
-    it("Should receive and store the funds to lock", async function () {
-      const { lock, lockedAmount } = await loadFixture(
-        deployOneYearLockFixture
-      );
+  // describe("getTodos", function () {
+  //   it("Should receive and store the funds to lock", async function () {
+  //     const { lock, lockedAmount } = await loadFixture(
+  //       deployOneYearLockFixture
+  //     );
 
-      expect(await ethers.provider.getBalance(lock.address)).to.equal(
-        lockedAmount
-      );
-    });
-  });
+  //     expect(await ethers.provider.getBalance(lock.address)).to.equal(
+  //       lockedAmount
+  //     );
+  //   });
+  // });
 
-  describe("deleteTodo", function () {
-    it("Should receive and store the funds to lock", async function () {
-      const { lock, lockedAmount } = await loadFixture(
-        deployOneYearLockFixture
-      );
+  // describe("deleteTodo", function () {
+  //   it("Should receive and store the funds to lock", async function () {
+  //     const { lock, lockedAmount } = await loadFixture(
+  //       deployOneYearLockFixture
+  //     );
 
-      expect(await ethers.provider.getBalance(lock.address)).to.equal(
-        lockedAmount
-      );
-    });
-  });
-});
+  //     expect(await ethers.provider.getBalance(lock.address)).to.equal(
+  //       lockedAmount
+  //     );
+  //   });
+  // });
+})
